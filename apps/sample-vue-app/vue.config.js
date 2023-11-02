f8a9c3e9abe2f2e9c09bcd716dba0d30364befbf
Darren Kelly <darren.kelly@vechain.org>
2023-11-02 22:03:55 +0000

    feat(gh-pages): publish dapps

--- apps/sample-vue-app/vue.config.js
+++ apps/sample-vue-app/vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 module.exports = defineConfig({
+    publicPath:
+        process.env.NODE_ENV === 'production' ? '/vechain-dapp-kit/vue/' : '/',
     transpileDependencies: true,
     configureWebpack: {
         plugins: [new NodePolyfillPlugin()],
