4877ecfb268362f9a5822f788a380f1aec4e1563
Davide Carpini <carpini.davide@gmail.com>
2023-11-23 11:26:47 +0100

    fix: add node polyfill plugin

--- apps/sample-svelte-app/src/lib/index.ts
+++ apps/sample-svelte-app/src/lib/index.ts
@@ -1,22 +1,19 @@
-// place files you want to import through the `$lib` alias in this folder.
-import '@vechainfoundation/dapp-kit-ui';
-// TODO: I didn't managed to let it work
-// import { DAppKit } from '@vechainfoundation/dapp-kit-ui';
+import { DAppKit } from '@vechainfoundation/dapp-kit-ui';
 
-// const walletConnectOptions = {
-// 	projectId: 'a0b855ceaf109dbc8426479a4c3d38d8',
-// 	metadata: {
-// 		name: 'Sample VeChain dApp',
-// 		description: 'A sample VeChain dApp',
-// 		url: window.location.origin,
-// 		icons: [`${window.location.origin}/images/logo/my-dapp.png`]
-// 	}
-// };
+const walletConnectOptions = {
+    projectId: 'a0b855ceaf109dbc8426479a4c3d38d8',
+    metadata: {
+        name: 'Sample VeChain dApp',
+        description: 'A sample VeChain dApp',
+        url: window.location.origin,
+        icons: [`${window.location.origin}/images/logo/my-dapp.png`],
+    },
+};
 
-// const vechainWalletKitOptions = {
-// 	nodeUrl: 'https://testnet.vechain.org/',
-// 	network: 'test',
-// 	walletConnectOptions
-// };
+const vechainWalletKitOptions = {
+    nodeUrl: 'https://testnet.vechain.org/',
+    network: 'test',
+    walletConnectOptions,
+};
 
-// const connex = DAppKit.configure(vechainWalletKitOptions);
+DAppKit.configure(vechainWalletKitOptions);
--- apps/sample-svelte-app/vite.config.ts
+++ apps/sample-svelte-app/vite.config.ts
@@ -1,12 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    resolve: {
-        alias: {
-            http: 'stream-http',
-            https: 'https-browserify',
-        },
-    },
+    plugins: [nodePolyfills(), sveltekit()],
 });
