2a098eadd44749b31afc267556a27d8ee32b85c9
Darren Kelly <darren.kelly@vechain.org>
2023-11-27 14:19:48 +0000

    fix: node version

--- apps/sample-next-app/src/app/page.tsx
+++ apps/sample-next-app/src/app/page.tsx
@@ -1,9 +1,20 @@
 'use client'; // This is a client component
 
-import { ConnectWalletButtonWithModal } from '@vechainfoundation/dapp-kit-react';
+import {
+    ConnectWalletButtonWithModal,
+    useWallet,
+} from '@vechainfoundation/dapp-kit-react';
+import React, { useEffect } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type, import/no-default-export
 export default function Home() {
+    const { account } = useWallet();
+
+    useEffect(() => {
+        // eslint-disable-next-line no-console
+        console.log('account', account);
+    }, [account]);
+
     return (
         <main>
             <ConnectWalletButtonWithModal />
