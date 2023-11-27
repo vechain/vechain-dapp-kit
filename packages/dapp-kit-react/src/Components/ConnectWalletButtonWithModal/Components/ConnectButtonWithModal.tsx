92a0f00f5ecf9cddd44de2e0ea35da8a977025d7
Darren Kelly <107671032+darrenvechain@users.noreply.github.com>
2023-11-27 20:44:14 +0000

    fix: create component dynamically (#105)

--- packages/dapp-kit-react/src/Components/ConnectWalletButtonWithModal/Components/ConnectButtonWithModal.tsx
+++ packages/dapp-kit-react/src/Components/ConnectWalletButtonWithModal/Components/ConnectButtonWithModal.tsx
@@ -1,12 +1,14 @@
-import { useCallback, useContext } from 'react';
+import { useCallback, useContext, useMemo } from 'react';
 import type { WalletSource } from '@vechainfoundation/dapp-kit';
 import type { SourceInfo } from '@vechainfoundation/dapp-kit-ui';
 import { ThemeContext } from '../../../provider/ThemeProvider';
 import { useWallet } from '../../../ConnexProvider';
-import { ConnectModalWithButtonWrapped } from './Wrapped/ConnectModalWithButtonWrapped';
+import { createButtonWithModal } from './Wrapped/ConnectModalWithButtonWrapped';
 
 export const ConnectButtonWithModal = () => {
     const { theme } = useContext(ThemeContext);
+
+    const ModalWithButton = useMemo(() => createButtonWithModal(), []);
 
     const { setSource, connect, setAccount } = useWallet();
 
@@ -32,12 +34,5 @@ export const ConnectButtonWithModal = () => {
         [connectHandler],
     );
 
-    return (
-        <>
-            <ConnectModalWithButtonWrapped
-                mode={theme.mode}
-                onSourceClick={_connect}
-            />
-        </>
-    );
+    return <ModalWithButton mode={theme.mode} onSourceClick={_connect} />;
 };
