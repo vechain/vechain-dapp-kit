57e2c929dede53bf6e6eac17024f10bcb60e186a
Darren Kelly <darren.kelly@vechain.org>
2023-11-29 13:02:00 +0000

    fix: dist instead of src

--- packages/dapp-kit-ui/index.js
+++ packages/dapp-kit-ui/index.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line eslint-comments/disable-enable-pair
 /* eslint-disable no-undef */
-import { DAppKit } from './src';
+import { DAppKit } from './dist';
 
 const walletConnectOptions = {
     projectId: 'a0b855ceaf109dbc8426479a4c3d38d8',
