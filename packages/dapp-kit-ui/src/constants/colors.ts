5254c8de7283e029dee98e74047edfc4f18db81a
Davide Carpini <carpini.davide@gmail.com>
2023-11-30 17:10:12 +0100

    fix: colors

--- packages/dapp-kit-ui/src/constants/colors.ts
+++ packages/dapp-kit-ui/src/constants/colors.ts
@@ -2,14 +2,14 @@ import { css } from 'lit';
 
 export const Colors = {
     White: css`#ffffff`,
-    XXLightGrey: css`#f4f4f4`,
-    XLightGrey: css`#f2f2f2`,
-    LightGrey: css`#f0f0f0`,
+    XXLightGrey: css`#f2f2f2`,
+    XLightGrey: css`#eeeeee`,
+    LightGrey: css`#eaeaea`,
     Grey: css`#888888`,
-    DarkGrey: css`#444444`,
-    XDarkGrey: css`#424242`,
+    DarkGrey: css`#4a4a4a`,
+    XDarkGrey: css`#454545`,
     XXDarkGrey: css`#404040`,
-    XXLightBlack: css`#343434`,
-    XLightBlack: css`#323232`,
-    LightBlack: css`#303030`,
+    XXLightBlack: css`#353535`,
+    XLightBlack: css`#303030`,
+    LightBlack: css`#2a2a2a`,
 };
