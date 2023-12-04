63523e549683f04e3f14cf386bf017f6f2ffd4d7
Davide Carpini <carpini.davide@gmail.com>
2023-12-04 19:55:58 +0100

    fix: colors comments

--- packages/dapp-kit-ui/src/constants/colors.ts
+++ packages/dapp-kit-ui/src/constants/colors.ts
@@ -1,23 +1,23 @@
 import { css } from 'lit';
 
 export const Colors = {
-    WalletConnectBlue: css`#3496ff`, // var(--vwk-color-walletconnectblue)
+    WalletConnectBlue: css`#3496ff`, // --vwk-color-walletconnectblue
 
     Dark: {
-        Primary: css`#404040`, // var(--vwk-color-dark-primary)
-        PrimaryHover: css`#454545`, // var(--vwk-color-dark-primary-hover)
-        PrimaryActive: css`#4a4a4a`, // var(--vwk-color-dark-primary-active)
-        Secondary: css`#2a2a2a`, // var(--vwk-color-dark-secondary)
-        Tertiary: css`#ffffff`, // var(--vwk-color-dark-tertiary)
-        Quaternary: css`#888888`, // var(--vwk-color-dark-quaternary)
+        Primary: css`#404040`, // --vwk-color-dark-primary
+        PrimaryHover: css`#454545`, // --vwk-color-dark-primary-hover
+        PrimaryActive: css`#4a4a4a`, // --vwk-color-dark-primary-active
+        Secondary: css`#2a2a2a`, // --vwk-color-dark-secondary
+        Tertiary: css`#ffffff`, // --vwk-color-dark-tertiary
+        Quaternary: css`#888888`, // --vwk-color-dark-quaternary
     },
 
     Light: {
-        Primary: css`#f2f2f2`, // var(--vwk-color-light-primary)
-        PrimaryHover: css`#eeeeee`, // var(--vwk-color-light-primary-hover)
-        PrimaryActive: css`#eaeaea`, // var(--vwk-color-light-primary-active)
-        Secondary: css`#ffffff`, // var(--vwk-color-light-secondary)
-        Tertiary: css`#2a2a2a`, // var(--vwk-color-light-tertiary)
-        Quaternary: css`#747474`, // var(--vwk-color-dark-quaternary)
+        Primary: css`#f2f2f2`, // --vwk-color-light-primary
+        PrimaryHover: css`#eeeeee`, // --vwk-color-light-primary-hover
+        PrimaryActive: css`#eaeaea`, // --vwk-color-light-primary-active
+        Secondary: css`#ffffff`, // --vwk-color-light-secondary
+        Tertiary: css`#2a2a2a`, // --vwk-color-light-tertiary
+        Quaternary: css`#747474`, // --vwk-color-dark-quaternary
     },
 };
