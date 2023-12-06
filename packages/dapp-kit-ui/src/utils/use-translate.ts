888e687a0a3c545381203cd9ce1717d00668e330
Darren Kelly <darren.kelly@vechain.org>
2023-12-06 09:46:00 +0000

    fix: broken tests

--- packages/dapp-kit-ui/src/utils/use-translate.ts
+++ packages/dapp-kit-ui/src/utils/use-translate.ts
@@ -3,5 +3,15 @@ import type { Language, TranslationKey, I18n } from '../constants';
 export const useTranslate =
     (i18n: I18n, language: Language) =>
     (s: TranslationKey): string => {
-        return i18n[language][s];
+        let _language = language;
+
+        if (!i18n[_language]) {
+            _language = 'en';
+        }
+
+        if (!i18n[_language][s]) {
+            return s;
+        }
+
+        return i18n[_language][s];
     };
