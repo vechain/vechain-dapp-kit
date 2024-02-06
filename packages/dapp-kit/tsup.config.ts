b793edc7dd37ae28f4ca6b0ed7a928c98f274367
Davide Carpini <carpini.davide@gmail.com>
2024-02-06 15:47:18 +0100

    fix: typescript configurations

---
+++ packages/dapp-kit/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+    entry: ['src/index.ts'],
+    outDir: 'dist',
+    format: 'esm',
+    minify: true,
+    sourcemap: true,
+    dts: true,
+    clean: true,
+});
