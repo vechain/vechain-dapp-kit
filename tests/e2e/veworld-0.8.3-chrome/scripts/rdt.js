d8bb2ebe4608aebdf58a7b2b7e3a62d5add87ab9
Darren Kelly <107671032+darrenvechain@users.noreply.github.com>
2024-03-26 11:28:37 +0000

    feat: run e2e tests in pipeline (#221)

--- tests/e2e/veworld-0.8.3-chrome/scripts/rdt.js
+++
@@ -1,5 +0,0 @@
-if (chrome.extension) {
-    const devToolsScript = document.createElement('script');
-    devToolsScript.src = 'react-devtools.js';
-    document.head.appendChild(devToolsScript);
-}
