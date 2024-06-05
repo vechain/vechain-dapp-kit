13e78584ff4dd002cc45366b7c46715c257389bd
Darren Kelly <darren.kelly@vechain.org>
2024-06-05 09:57:31 +0100

    fix: add a hack for the provider

--- examples/sample-react-app/src/hooks/useCounter.ts
+++ examples/sample-react-app/src/hooks/useCounter.ts
@@ -1,14 +1,15 @@
 import { useProvider } from '@vechain/dapp-kit-react';
-import { useMemo } from 'react';
 import { Counter__factory } from '@vechain/hardhat-example';
 
 const counterAddress = '0x8384738c995d49c5b692560ae688fc8b51af1059';
 
 export const useCounter = () => {
     const provider = useProvider();
-    const signer = useMemo(() => {
-        return provider.getSignerSync();
-    }, [provider]);
 
-    return Counter__factory.connect(counterAddress, signer);
+    return Counter__factory.connect(
+        counterAddress,
+        // TODO: Fix this hack
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        provider.getSignerSync() as unknown as any,
+    );
 };
