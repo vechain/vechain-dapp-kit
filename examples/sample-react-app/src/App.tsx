0f97d05855ee9f602f6fbaa20a6fca7687c27f2b
Valazan <al-fede@hotmail.it>
2024-10-03 16:44:46 +0200

    fix: linting

--- examples/sample-react-app/src/App.tsx
+++ examples/sample-react-app/src/App.tsx
@@ -6,47 +6,9 @@ import {
 import { useEffect, useState } from 'react';
 
 function App() {
-    const { account, signTypedData } = useWallet();
+    const { account } = useWallet();
     const { open, onConnectionStatusChange } = useWalletModal();
     const [buttonText, setButtonText] = useState('Connect Custom Button');
-
-    // All properties on a domain are optional
-    const domain = {
-        name: 'Ether Mail',
-        version: '1',
-        chainId: 1,
-        verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
-    };
-
-    // The named list of all type definitions
-    const types = {
-        Person: [
-            { name: 'name', type: 'string' },
-            { name: 'wallet', type: 'address' },
-        ],
-        Mail: [
-            { name: 'from', type: 'Person' },
-            { name: 'to', type: 'Person' },
-            { name: 'contents', type: 'string' },
-        ],
-    };
-
-    // The data to sign
-    const value = {
-        from: {
-            name: 'Cow',
-            wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
-        },
-        to: {
-            name: 'Bob',
-            wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
-        },
-        contents: 'Hello, Bob!',
-    };
-
-    const signTypedDataHandler = async () => {
-        signTypedData(domain, types, value);
-    };
 
     useEffect(() => {
         const handleConnected = (address: string | null) => {
@@ -70,7 +32,6 @@ function App() {
         <div className="container">
             <h2>React JS</h2>
             <div className="label">kit button:</div>
-            <button onClick={signTypedDataHandler}>Sign Typed Data</button>
             <WalletButton />
             <div className="label">custom button:</div>
             <button onClick={open}>{buttonText}</button>
