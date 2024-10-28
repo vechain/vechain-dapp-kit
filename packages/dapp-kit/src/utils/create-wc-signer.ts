7c40e7da032d0c76f03f17d4a14373f1ba5a80c4
Darren Kelly <107671032+darrenvechain@users.noreply.github.com>
2024-10-28 08:28:11 +0000

    feat: ping wallet connect on focus (#224)
    * feat: ping wallet connect on focus
    
    * fix: if statement for ssr
    
    * fix: update yarn.lock
    
    ---------
    
    Co-authored-by: Fabio Rigamonti <fabio.rigamonti@icloud.com>
    Co-authored-by: Davide Carpini <carpini.davide@gmail.com>

--- packages/dapp-kit/src/utils/create-wc-signer.ts
+++ packages/dapp-kit/src/utils/create-wc-signer.ts
@@ -41,6 +41,20 @@ export const createWcSigner = ({
             throw new Error(`Failed to get the wallet connect sign client`);
         });
 
+    /**
+     * Ping the session when the window is focused
+     * if (window) to prevent SSR errors
+     */
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (window) {
+        window.onfocus = async (): Promise<void> => {
+            if (session) {
+                const signClient = await wcClient.get();
+                await signClient.ping({ topic: session.topic });
+            }
+        };
+    }
+
     // listen for session updates
     const listenToEvents = (_client: SignClient): void => {
         _client.on('session_update', ({ topic, params }): void => {
