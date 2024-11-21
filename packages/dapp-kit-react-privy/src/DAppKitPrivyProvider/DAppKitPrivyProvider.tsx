f2e74cbf905454ea223904a4e9784021f889e8d5
Fabio Rigamonti <73019897+fabiorigam@users.noreply.github.com>
2024-11-21 17:46:27 +0100

    fix: continue the privy provider

--- packages/dapp-kit-react-privy/src/DAppKitPrivyProvider/DAppKitPrivyProvider.tsx
+++ packages/dapp-kit-react-privy/src/DAppKitPrivyProvider/DAppKitPrivyProvider.tsx
@@ -1,8 +1,67 @@
+import { ReactNode } from "react";
 import { PrivyProvider as BasePrivyProvider } from "@privy-io/react-auth";
 
-export const DAppKitPrivyProvider = ({}) => {
+type Props = {
+  children: ReactNode;
+  appId: string;
+  clientId: string;
+  appearance: {
+    theme: "dark" | "light";
+    accentColor: `#${string}`;
+    loginMessage: string;
+    logo: string;
+  };
+  embeddedWallets: {
+    createOnLogin: "all-users";
+  };
+  loginMethods: (
+    | "wallet"
+    | "email"
+    | "sms"
+    | "google"
+    | "twitter"
+    | "discord"
+    | "github"
+    | "linkedin"
+    | "spotify"
+    | "instagram"
+    | "tiktok"
+    | "apple"
+    | "farcaster"
+    | "telegram"
+  )[];
+  smartAccountConfig: {
+    nodeUrl: string;
+    delegatorUrl: string;
+    accountFactoryAddress: string;
+  };
+  dappKitConfig: {
+    nodeUrl: string;
+    genesis: Connex.Thor.Block;
+    projectId: string;
+    colorMode: "DARK" | "LIGHT";
+    walletConnectOptions: {
+      projectId: string;
+      metadata: {
+        name: string;
+        description: string;
+        url: string;
+        icons: string[];
+      };
+    };
+  };
+};
+
+export const DAppKitPrivyProvider = ({
+  children,
+  appId,
+  clientId,
+  loginMethods,
+  appearance,
+  embeddedWallets,
+}: Props) => {
     return (
-        <BasePrivyProvider
+      <BasePrivyProvider  
         appId={appId}
         clientId={clientId}
         config={{
@@ -13,9 +72,7 @@ export const DAppKitPrivyProvider = ({}) => {
           },
         }}
       >
-        <DAppKitProvider>
-            {children}
-        </DAppKitProvider>
+        {children}
       </BasePrivyProvider>
     );
 };
--- packages/dapp-kit-react-privy/src/DAppKitPrivyProvider/index.ts
+++ packages/dapp-kit-react-privy/src/DAppKitPrivyProvider/index.ts
@@ -1,2 +1,1 @@
 export * from './DAppKitPrivyProvider';
-export * from './hooks';
