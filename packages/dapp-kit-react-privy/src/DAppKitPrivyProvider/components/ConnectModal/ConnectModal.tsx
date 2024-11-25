06c49d8de0418a6c1c125f77ae8076564aa90b78
Fabio Rigamonti <73019897+fabiorigam@users.noreply.github.com>
2024-11-25 12:40:21 +0100

    fix: improve modal

--- packages/dapp-kit-react-privy/src/DAppKitPrivyProvider/components/ConnectModal/ConnectModal.tsx
+++ packages/dapp-kit-react-privy/src/DAppKitPrivyProvider/components/ConnectModal/ConnectModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Dialog, Transition } from "@headlessui/react";
+import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from "@headlessui/react";
 import { Fragment } from "react";
 import { usePrivy } from "@privy-io/react-auth";
 import { useWalletModal } from "@vechain/dapp-kit-react";
@@ -20,7 +20,7 @@ export const ConnectModal = ({ isOpen, onClose, logo }: Props) => {
   return (
     <Transition appear show={isOpen} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={onClose}>
-        <Transition.Child
+        <TransitionChild
           as={Fragment}
           enter="ease-out duration-300"
           enterFrom="opacity-0"
@@ -30,11 +30,11 @@ export const ConnectModal = ({ isOpen, onClose, logo }: Props) => {
           leaveTo="opacity-0"
         >
           <div className="fixed inset-0 bg-black bg-opacity-25" />
-        </Transition.Child>
+        </TransitionChild>
 
         <div className="fixed inset-0 overflow-y-auto">
           <div className="flex items-center justify-center min-h-full p-4 text-center">
-            <Transition.Child
+            <TransitionChild
               as={Fragment}
               enter="ease-out duration-300"
               enterFrom="opacity-0 scale-95"
@@ -43,13 +43,13 @@ export const ConnectModal = ({ isOpen, onClose, logo }: Props) => {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
-                <Dialog.Title
+              <DialogPanel className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                <DialogTitle
                   as="h3"
                   className="text-lg font-medium leading-6 text-gray-900 text-center"
                 >
                   Log in or sign up
-                </Dialog.Title>
+                </DialogTitle>
 
                 <div className="mt-4 flex justify-center">
                   <img
@@ -98,8 +98,8 @@ export const ConnectModal = ({ isOpen, onClose, logo }: Props) => {
                     </button>
                   </div>
                 </div>
-              </Dialog.Panel>
-            </Transition.Child>
+              </DialogPanel>
+            </TransitionChild>
           </div>
         </div>
       </Dialog>
