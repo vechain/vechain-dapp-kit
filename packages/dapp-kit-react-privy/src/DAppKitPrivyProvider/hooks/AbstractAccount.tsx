2e4ffe3dc7a8ccca5e9dd3238c66d376fbbbb777
Fabio Rigamonti <73019897+fabiorigam@users.noreply.github.com>
2024-12-18 12:50:56 +0100

    fix: fix import

--- packages/dapp-kit-react-privy/src/DAppKitPrivyProvider/hooks/AbstractAccount.tsx
+++ packages/dapp-kit-react-privy/src/DAppKitPrivyProvider/hooks/AbstractAccount.tsx
@@ -1,11 +1,13 @@
 import { ABIContract } from '@vechain/sdk-core';
 import { THOR_CLIENT } from '../utils/Constants';
-import { SimpleAccountABI } from '../assets';
+import { SimpleAccountFactoryABI } from '../assets';
 
 export const getAbstractAddress = async (address: string): Promise<string> => {
     const account = await THOR_CLIENT.contracts.executeCall(
         process.env.NEXT_PUBLIC_AA_FACTORY as string,
-        ABIContract.ofAbi(SimpleAccountABI).getFunction('getAccountAddress'),
+        ABIContract.ofAbi(SimpleAccountFactoryABI).getFunction(
+            'getAccountAddress',
+        ),
         [address],
     );
 
