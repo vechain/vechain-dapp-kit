5ddcdab60fb167568e637556d823b1bba03e529f
Dan Rusnac <rusnac14@gmail.com>
2024-12-24 17:53:16 +0100

    fix: merge conflicts

--- packages/dapp-kit-react-privy/src/DAppKitPrivyProvider/components/common/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './FadeInViewFromRight';
-export * from './FadeInViewFromBottom';
-export * from './ModalBackButton';
-export * from './AddressDisplay';
-export * from './AppLogos';
-export * from './AccountSelector';
--- packages/dapp-kit-react-privy/src/assets/index.ts
+++ packages/dapp-kit-react-privy/src/assets/index.ts
@@ -1,1 +1,2 @@
 export * from './abi';
+export * from './icons';
