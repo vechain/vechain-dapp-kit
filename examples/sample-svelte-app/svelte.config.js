254ec9b2da1fddd5455ff8956aa364001266f670
Darren Kelly <darren.kelly@vechain.org>
2025-04-08 08:44:11 +0100

    fix: build issues

--- examples/sample-svelte-app/svelte.config.js
+++ examples/sample-svelte-app/svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 const basePath = process.env.BASE_PATH ?? '';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://kit.svelte.dev/docs/integrations#preprocessors
     // for more information about preprocessors
-    preprocess: vitePreprocess(),
-
+    preprocess: {
+        postcss: true,
+    },
     kit: {
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
