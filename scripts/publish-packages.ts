0fea3d1e139c2d8063a97b68ac68d4046f68c80d
Darren Kelly <107671032+darrenvechain@users.noreply.github.com>
2025-05-12 11:40:40 +0100

    V2.0.0 rc.4 (#363)
    * chore(release): v2.0.0-rc.4

--- scripts/publish-packages.ts
+++ scripts/publish-packages.ts
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
 import util from 'util';
 import * as child_process from 'child_process';
-import fs from "fs"
-import path from "path"
+import fs from 'fs';
+import path from 'path';
 
 const exec = util.promisify(child_process.exec);
 
@@ -10,47 +10,48 @@ const exec = util.promisify(child_process.exec);
 const packages = fs.readdirSync(path.resolve(__dirname, '../packages'));
 
 const updatePackageVersions = (version: string) => {
-    const packageNames = [];
-
     for (const pkg of packages) {
         const pkgPath = path.resolve(__dirname, `../packages/${pkg}`);
         const pkgJsonPath = path.resolve(pkgPath, './package.json');
         const pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf8'));
-        pkgJson.version = version;
-        packageNames.push(pkgJson.name);
+        if (pkgJson.version !== version) {
+            throw new Error(`Tag/ package.json mismatch ${pkgJsonPath}`);
+        }
+        for (const dep of Object.keys(pkgJson.dependencies || {})) {
+            if (dep.includes('@vechain/dapp-kit')) {
+                pkgJson.dependencies[dep] = version;
+            }
+        }
         fs.writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, 2));
     }
 };
 
 const publishPackages = async () => {
-    const version = process.argv[2];
+    if (!process.env.NODE_AUTH_TOKEN) {
+        console.error(
+            `🚨 You must set the NODE_AUTH_TOKEN environment variable 🚨`,
+        );
+        process.exit(1);
+    }
+
+    const { stdout } = await exec('git rev-parse --abbrev-ref HEAD');
+
+    const version = stdout.trim();
+
+    console.log(`Version: ${version}`);
 
     if (
-      !version ||
-      (!version.match(/^\d+\.\d+\.\d+$/) &&
-        !version.match(/^\d+\.\d+\.\d+(-rc\.\d+)?$/))
+        !version ||
+        (!version.match(/^\d+\.\d+\.\d+$/) &&
+            !version.match(/^\d+\.\d+\.\d+(-rc\.\d+)?$/))
     ) {
-        console.error(
-          `🚨 You must specify a semantic version as the first argument  🚨`,
-        );
+        console.error(`🚨 Branch name is not a valid tag (${version}) 🚨\n`);
         process.exit(1);
     }
 
     console.log('\n______________________________________________________\n\n');
     console.log(`   🚀🚀🚀 Publishing ${version}  🚀🚀🚀`);
     console.log('\n______________________________________________________\n\n');
-
-    console.log(' Checkout to main:');
-    await exec('git checkout main');
-    console.log('       - ✅  Checked out!');
-
-    console.log(' Pull the tag:');
-    await exec('git pull');
-    console.log('       - ✅  Pulled!');
-
-    console.log(' Checkout the tag:');
-    await exec(`git checkout ${version}`);
-    console.log('       - ✅  Checked out!');
 
     console.log(' Clean:');
     console.log('       - 🚮 Purging node_modules...');
@@ -82,6 +83,10 @@ const publishPackages = async () => {
             {
                 stdio: 'inherit', // This allows interaction with the process's stdio
                 shell: true, // Enables shell features if needed
+                env: {
+                    ...process.env,
+                    NODE_AUTH_TOKEN: process.env.NODE_AUTH_TOKEN,
+                },
             },
         );
 
