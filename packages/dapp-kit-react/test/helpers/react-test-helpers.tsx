f1f28095672bb8ec278ec32d20acec6fececeb7c
HiiiiD <salomone.marco@outlook.it>
2025-08-28 17:15:02 +0200

    fix: final fixes

--- packages/dapp-kit-react/test/helpers/react-test-helpers.tsx
+++ packages/dapp-kit-react/test/helpers/react-test-helpers.tsx
@@ -1,8 +1,14 @@
+import { TESTNET_NETWORK } from '@vechain/sdk-core';
 import React from 'react';
-import { WalletButton, DAppKitProvider } from '../../src';
+import { DAppKitProvider, WalletButton } from '../../src';
 
 export const wrapper = ({ children }: { children?: React.ReactNode }) => (
-    <DAppKitProvider node="https://testnet.vechain.org" logLevel={'DEBUG'}>
+    <DAppKitProvider
+        node="https://testnet.vechain.org"
+        logLevel={'DEBUG'}
+        v2Api={{ enabled: false }}
+        genesisId={TESTNET_NETWORK.genesisBlock.id}
+    >
         {children}
         <WalletButton />
     </DAppKitProvider>
