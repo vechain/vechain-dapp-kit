35324262fdea093bec4ac116d3ef42bb1eeb66a5
HiiiiD <salomone.marco@outlook.it>
2025-10-13 09:02:44 +0200

    feat: showcase different way of signing in

--- examples/sample-react-app/src/main.tsx
+++ examples/sample-react-app/src/main.tsx
@@ -1,31 +1,14 @@
-import type { WalletConnectOptions } from '@vechain/dapp-kit';
-import { DAppKitProvider } from '@vechain/dapp-kit-react';
 import '@vechain/dapp-kit-ui';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
-const walletConnectOptions: WalletConnectOptions = {
-    projectId: 'a0b855ceaf109dbc8426479a4c3d38d8',
-    metadata: {
-        name: 'Sample VeChain dApp',
-        description: 'A sample VeChain dApp',
-        url: window.location.origin,
-        icons: [`${window.location.origin}/images/logo/my-dapp.png`],
-    },
-};
+import { Provider } from './provider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <DAppKitProvider
-            node={'https://testnet.vechain.org'}
-            usePersistence
-            walletConnectOptions={walletConnectOptions}
-            logLevel={'DEBUG'}
-            v2Api={{ enabled: true }}
-        >
+        <Provider>
             <App />
-        </DAppKitProvider>
+        </Provider>
     </React.StrictMode>,
 );
